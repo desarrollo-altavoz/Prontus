e2ad1ca72d7df91b72528613c4bbd13beae730ac
Cesar Vasquez <cvasquez@altavoz.net>
2013-01-07 11:55:35 -0300

    Script generar_release.sh terminado

--- wizard_prontus/release/generar_release.sh
+++ wizard_prontus/release/generar_release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#~ A futuro, leer esto desde el prontus_varglb.pm
 echo "Ingrese la release (ej: 11.2.31):"
 read -r release
 
@@ -8,17 +9,34 @@ if [ "$release" = "" ] ; then
     exit
 fi
 
-cd "prontus.$release/"
+SCRIPT=`readlink -f $0`
+SCRIPTPATH=`dirname $SCRIPT`
+SCRIPTPATH=`dirname $SCRIPTPATH`
+BASEDIR=`dirname $SCRIPTPATH`
+RELEASEPATH="$BASEDIR/release"
+
+if [ ! -d "$RELEASEPATH" ] ; then
+    mkdir "$RELEASEPATH";
+else
+    rm -rf "$RELEASEPATH/*";
+fi
 
 echo "Generando archivo tgz"
-tar czf "files.$release.tgz"  cgi-bin/ cgi-cpn/ wizard_prontus/core/ wizard_prontus/prontus_dir/cpan/core/
+cd $BASEDIR
+tar czf "$RELEASEPATH/files.$release.tgz" --exclude=*.log cgi-bin/ cgi-cpn/ wizard_prontus/core/ wizard_prontus/prontus_dir/cpan/core/
 
 echo "Generando md5"
-/sbin/md5 "files.$release.tgz" > "files.$release.tgz.md5"
-
-if [ ! -f "models.$release.tgz" ] ; then
-    echo "Generando modelos"
-    tar czf "models.$release.tgz" wizard_prontus/models wizard_prontus/models_lib
+cd $RELEASEPATH
+if [ -x /sbin/md5 ] ; then
+    /sbin/md5 "files.$release.tgz" > "$RELEASEPATH/files.$release.tgz.md5"
+else
+    /usr/bin/openssl dgst -md5 "files.$release.tgz" > "$RELEASEPATH/files.$release.tgz.md5"
 fi
+
+#~ Por ahora no se distribuiran mas los modelos
+#~ if [ ! -f "models.$release.tgz" ] ; then
+    #~ echo "Generando modelos"
+    #~ tar czf "models.$release.tgz" wizard_prontus/models wizard_prontus/models_lib
+#~ fi
 
 echo "Archivos generados ok"
