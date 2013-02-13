e4df3310abd602b4be467a1673d4bb49afc6ac9e
Cesar Vasquez <cvasquez@altavoz.net>
2013-02-13 10:38:23 -0300

    Preparacion para nueva release 11.2.58

--- cgi-cpn/prontus_varglb.pm
+++ cgi-cpn/prontus_varglb.pm
@@ -46,7 +46,7 @@ sub init { # Prontus 6.0
   # $DIR_CGI_PUBLIC = 'cgi-bin'; # 1.13
   require 'dir_cgi.pm';
 
-  $VERSION_PRONTUS = '11.2.57 - 06/02/2013';
+  $VERSION_PRONTUS = '11.2.58 - 13/02/2013';
   $RAMA_INSTALADA = '';
   $NRO_REVISION_INSTALADA = '';
   $BETA_REVISION_INSTALADA = '';
