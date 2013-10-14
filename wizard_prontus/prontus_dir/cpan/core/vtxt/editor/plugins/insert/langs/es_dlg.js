c2e8547c91397435a5f83109353ba1b733ab3e16
Cesar Vasquez <cvasquez@altavoz.net>
2013-10-14 18:43:04 -0300

    Se corrigen textos en el nuevo plugin Insert

--- wizard_prontus/prontus_dir/cpan/core/vtxt/editor/plugins/insert/langs/es_dlg.js
+++ wizard_prontus/prontus_dir/cpan/core/vtxt/editor/plugins/insert/langs/es_dlg.js
@@ -1,12 +1,12 @@
 tinyMCE.addI18n('es.insert_dlg',{
         title : 'Insertar HTML o Javascript',
-        insert_desc: 'La representanción en el VTXT es sólo gráfica, a modo de ayuda, ya que dependiendo del HTML incluído el resultado podría ser diferente. No coloque 2 o más bloques "Insert" seguidos, porque el editor dejará sólo el primero.',
+        insert_desc: 'La representación en el VTXT es sólo gráfica, a modo de ayuda, ya que dependiendo del HTML incluído el resultado podría ser diferente. No coloque 2 o más bloques "Insert" seguidos, porque el editor dejará sólo el primero.',
         insert_important: 'Importante:',
 
 
         tab_html: 'Insertar un Archivo',
         html_desc1: 'En la mayoría de los casos la detección automática del tipo de include funcionará sola. Si no, deberá indicar el tipo de include que usa su sitio web',
-        html_desc2: 'Luego indique la ruta absoluta al archivo que desea incluír',
+        html_desc2: 'Luego indique la ruta absoluta al archivo que desea incluir',
         html_type: 'Tipo de include a realizar',
         html_type_auto: 'Automatico',
         html_type_php: 'Forzar Include PHP',
@@ -14,7 +14,7 @@ tinyMCE.addI18n('es.insert_dlg',{
         html_file: 'Ruta al archivo',
 
         tab_js: 'Insertar código Javascript',
-        js_desc1: 'Seleccione el tipo de Include que desea realiza: Puede ser código o un script JS externo',
+        js_desc1: 'Seleccione el tipo de Include que desea realizar: Puede ser código o un script JS externo',
         js_desc2: 'Tenga cuidado de no ingresar código con errores, ya que el artículo podría funcionar de manera incorrecta o perder funcionalidades',
         js_type: 'Tipo de Script a insertar',
         js_type_code: 'Código Javascript',
