d21e357cb59fb630b6221615c70e2c95a41548a1
Cesar Vasquez <cvasquez@altavoz.net>
2013-11-07 09:54:31 -0300

    Cambios para la release

--- cgi-cpn/prontus_varglb.pm
+++ cgi-cpn/prontus_varglb.pm
@@ -46,7 +46,7 @@ sub init { # Prontus 6.0
   # $DIR_CGI_PUBLIC = 'cgi-bin'; # 1.13
   require 'dir_cgi.pm';
 
-  $VERSION_PRONTUS = '11.2.70 - 01/10/2013';
+  $VERSION_PRONTUS = '11.2.71 - 07/11/2013';
   $RAMA_INSTALADA = '';
   $NRO_REVISION_INSTALADA = '';
   $BETA_REVISION_INSTALADA = '';
