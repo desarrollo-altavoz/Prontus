adf073005a52db3f9338c864e23186561ccaf584
Jorge Olivares <jorge.olivares.ro@gmail.com>
2014-11-26 10:23:31 -0300

    Cambios a change_log y actualizacion de fecha de release.

--- cgi-cpn/prontus_varglb.pm
+++ cgi-cpn/prontus_varglb.pm
@@ -46,7 +46,7 @@ sub init { # Prontus 6.0
   # $DIR_CGI_PUBLIC = 'cgi-bin'; # 1.13
   require 'dir_cgi.pm';
 
-  $VERSION_PRONTUS = '11.2.81 - 25/11/2014';
+  $VERSION_PRONTUS = '11.2.81 - 26/11/2014';
   $RAMA_INSTALADA = '';
   $NRO_REVISION_INSTALADA = '';
   $BETA_REVISION_INSTALADA = '';
