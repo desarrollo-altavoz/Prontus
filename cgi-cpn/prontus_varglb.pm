cb5a7207578f7a44fd44c2cd5416e175889ff237
Jorge Olivares <jorge.olivares.ro@gmail.com>
2015-11-13 12:40:52 -0300

    Actualización de release notes y cambio de versión.

--- cgi-cpn/prontus_varglb.pm
+++ cgi-cpn/prontus_varglb.pm
@@ -46,7 +46,7 @@ sub init { # Prontus 6.0
   # $DIR_CGI_PUBLIC = 'cgi-bin'; # 1.13
   require 'dir_cgi.pm';
 
-  $VERSION_PRONTUS = '11.2.88 - 04/11/2015';
+  $VERSION_PRONTUS = '11.2.88 - 13/11/2015';
   $RAMA_INSTALADA = '';
   $NRO_REVISION_INSTALADA = '';
   $BETA_REVISION_INSTALADA = '';
