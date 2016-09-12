337798cdce1a6af0355a869fcdc8cfcfa8c4fd63
Eduardo Alarcón <ealarcon@altavoz.net>
2016-09-12 11:26:36 -0300

    Actualización release notes y version para release.

--- cgi-cpn/prontus_varglb.pm
+++ cgi-cpn/prontus_varglb.pm
@@ -46,7 +46,7 @@ sub init { # Prontus 6.0
   # $DIR_CGI_PUBLIC = 'cgi-bin'; # 1.13
   require 'dir_cgi.pm';
 
-  $VERSION_PRONTUS = '11.2.92 - 08/09/2016';
+  $VERSION_PRONTUS = '11.2.92 - 12/09/2016';
   $RAMA_INSTALADA = '';
   $NRO_REVISION_INSTALADA = '';
   $BETA_REVISION_INSTALADA = '';
