b98153b241c8c4744dc001866d2d3ca779bea49e
Eduardo Alarcón <ealarcon@altavoz.net>
2017-03-10 17:30:22 -0300

    Se corrige error que permitia ingresar "-" al duplicar portada

--- wizard_prontus/prontus_dir/cpan/core/js-local/PortAdmin.class.js
+++ wizard_prontus/prontus_dir/cpan/core/js-local/PortAdmin.class.js
@@ -1,15 +1,15 @@
 
 var PortAdmin = {
-        
+
     duplicar: function () {
-    
+
         if ($('select[name="Lst_PORT3"] option:selected').length) {
             var newport = prompt('Plantilla Origen: ' + $('select[name="Lst_PORT3"] option:selected').val() + '\nIngrese el nombre de la nueva plantilla, ejemplo: politica.html', '');
             if (newport) {
-                var expr = /^[a-z\_0-9][a-z\_\-0-9]*\.[a-z\_0-9\-]+$/;
+                var expr = /^[a-z\_0-9]+\.[a-z\_0-9]+$/;
                 var found = expr.exec(newport);
                 if (!found) {
-                    alert('Nombre de plantilla no válido.\nDebe comenzar con letra, número o underscore.\nCaracteres permitidos:letras minúsculas, dígitos, guión y underscore más el punto para la extensión, la cual es obligatoria.');
+                    alert('Nombre de plantilla no válido.\nDebe comenzar con letra, número o underscore.\nCaracteres permitidos:letras minúsculas, dígitos y underscore más el punto para la extensión, la cual es obligatoria.');
                 } else {
                     $('input[name="NEW_PORT"]').val(newport);
                     var configAjax = {
@@ -33,14 +33,14 @@ var PortAdmin = {
         }
     },
     borrar: function () {
-    
+
         if ($('select[name="Lst_PORT4"] option:selected').length) {
             if (confirm("¿Está seguro de eliminar esta Portada?")) {
                 var configAjax = {
                     formSelector: '#frmBorrar',
                     actionURL: '/' + Intercambio.dir_cgi_cpan + '/prontus_pltport_borrar.cgi',
                 };
-                
+
                 var opts = {
                     complete: function () {
                         parent.Opciones.cerrarColorbox();
