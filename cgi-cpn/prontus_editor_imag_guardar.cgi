5e9e0907c199cd66bbfad1b12af66b143d662a38
jor <jolivares@altavoz.net>
2017-05-24 14:37:24 -0400

    Se guarda foto en banco de imagenes del articulo.

---
+++ cgi-cpn/prontus_editor_imag_guardar.cgi
@@ -0,0 +1,150 @@
+#!/usr/bin/perl
+
+# ---------------------------------------------------------------
+# Prontus CMS
+# http://www.prontus.cl
+# by Altavoz.net
+#
+# licensed under LGPL license.
+# http://www.prontus.cl/license.html
+# ---------------------------------------------------------------
+
+# BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA
+# BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA
+
+BEGIN {
+    use FindBin '$Bin';
+    $pathLibsProntus = $Bin;
+    unshift(@INC,$pathLibsProntus);
+};
+
+# Captura STDERR
+use lib_stdlog;
+&lib_stdlog::set_stdlog($0, 51200);
+
+use glib_cgi_04;
+use glib_fildir_02;
+use prontus_varglb; &prontus_varglb::init();
+use glib_html_02;
+use lib_prontus;
+use glib_str_02;
+use lib_thumb;
+use File::Copy;
+
+use strict;
+
+my (%FORM);
+
+main: {
+    # Rescatar parametros recibidos
+    &glib_cgi_04::new();
+    $FORM{'path_conf'}  = &glib_cgi_04::param('_path_conf');
+
+    # Parametros del editor.
+    $FORM{'foto'}           = &glib_cgi_04::param('foto');
+    $FORM{'srcX'}           = &glib_cgi_04::param('srcX');
+    $FORM{'srcY'}           = &glib_cgi_04::param('srcY');
+    $FORM{'width'}          = &glib_cgi_04::param('width');
+    $FORM{'height'}         = &glib_cgi_04::param('height');
+    $FORM{'rotate'}         = &glib_cgi_04::param('rotate');
+    $FORM{'aspectRatio'}    = &glib_cgi_04::param('aspectRatio');
+    $FORM{'zoomRatio'}      = &glib_cgi_04::param('zoomRatio');
+
+    # Ajusta path_conf para completar path y/o cambiar \ por /
+    $FORM{'path_conf'} = &lib_prontus::ajusta_pathconf($FORM{'path_conf'});
+
+    # Carga variables de configuracion.
+    &lib_prontus::load_config($FORM{'path_conf'});  # Prontus 6.0
+    $FORM{'path_conf'} =~ s/^$prontus_varglb::DIR_SERVER//;
+
+    # user check
+    ($prontus_varglb::USERS_ID, $prontus_varglb::USERS_PERFIL) = &lib_prontus::check_user();
+    if ($prontus_varglb::USERS_ID eq '') {
+        &glib_html_02::print_json_result(0, $prontus_varglb::USERS_PERFIL, 'exit=1,ctype=1');
+    }
+
+    # valida imag
+    if (!(-f $prontus_varglb::DIR_SERVER . $FORM{'foto'})) {
+        &glib_html_02::print_json_result(0, 'La imagen no es válida', 'exit=1,ctype=1');
+    }
+
+    # Dir de trabajo
+    my $prontus_id = $prontus_varglb::PRONTUS_ID;
+    my $reldir_imgedit = "/$prontus_id/cpan/procs/imgedit";
+    &glib_fildir_02::check_dir("$prontus_varglb::DIR_SERVER$reldir_imgedit");
+    # Garbage
+    &garbage_work_images("$prontus_varglb::DIR_SERVER$reldir_imgedit");
+
+    my $relpath_img_dst;
+    # Determinar si la imagen es la original o una tmp y segun eso determinar el path de la imagen de trabajo
+    if ($FORM{'foto'} =~ /^\/$prontus_id\/site\/.*?\/[0-9]{8}\/imag\/foto_([0-9]+)\.(\w+)$/) {
+        my $random = &glib_str_02::random_string(12);
+        my $ts = $1;
+        my $ext = $2; # sin punto
+
+        $relpath_img_dst = "$reldir_imgedit/foto_$ts" . '_' . "$random.$ext";
+        while (-f "$prontus_varglb::DIR_SERVER$relpath_img_dst") {
+            $random = &glib_str_02::random_string(12);
+            $relpath_img_dst = "$reldir_imgedit/foto_$ts" . '_' . "$random.$ext";
+        }
+        # genera imagen nueva tmp para trabajar sobre ella
+        &File::Copy::copy($prontus_varglb::DIR_SERVER . $FORM{'foto'}, "$prontus_varglb::DIR_SERVER$relpath_img_dst");
+    }  else {
+        # Invalida.
+        &glib_html_02::print_json_result(0, "foto mala", 'exit=1,ctype=1');
+    }
+
+    my $path_img_dst = "$prontus_varglb::DIR_SERVER$relpath_img_dst";
+
+    # Rotar antes de hacer crop.
+    if ($FORM{'rotate'}) {
+        my $degrees = $FORM{'rotate'};
+        # cropper.js lo entrega asi. hay que ajustarlo.
+        $degrees = 270 if ($degrees == -90);
+        $degrees = 180 if ($degrees == -180);
+        $degrees = 90 if ($degrees == -270);
+
+        my $binfoto = &lib_thumb::make_rotate($degrees, $path_img_dst);
+        $path_img_dst =~ s/\.gif$/\.png/i if($path_img_dst =~ /\.gif$/i);
+        &lib_thumb::write_image($path_img_dst, $binfoto);
+    }
+
+    print STDERR "path_img_dst[$path_img_dst]\n";
+    my ($binfoto, $final_w, $final_h) = &lib_thumb::make_crop($FORM{'srcX'}, $FORM{'srcY'}, $FORM{'width'}, $FORM{'height'}, $path_img_dst);
+
+    $path_img_dst =~ s/\.gif$/\.png/i if($path_img_dst =~ /\.gif$/i);
+    &lib_thumb::write_image($path_img_dst, $binfoto);
+
+    if ($FORM{'zoomRatio'} > 1) {
+        my $zoomW = $final_w * $FORM{'zoomRatio'};
+        my $zoomH = $final_h * $FORM{'zoomRatio'};
+
+        my ($binfoto, $anchofinal, $altofinal) = &lib_thumb::make_resize($zoomW, $zoomH, $path_img_dst);
+        $path_img_dst =~ s/\.gif$/\.png/i if($path_img_dst =~ /\.gif$/i);
+        &lib_thumb::write_image($path_img_dst, $binfoto);
+    }
+
+    # &glib_html_02::print_json_result(1, "fin.", 'exit=1,ctype=1');
+
+    &glib_html_02::print_json_result(1, "$relpath_img_dst", 'exit=1,ctype=1');
+
+    # print $pagina;
+};
+
+sub garbage_work_images {
+    my $dir = shift;
+    my $segs_antig = 28800; # 8hrs
+    # Lee el contenido del directorio.
+    my @lisdir = &glib_fildir_02::lee_dir($dir);
+    @lisdir = grep !/^\./, @lisdir; # Elimina directorios . y ..
+
+    # Borra archivos con mas $segs_antig de antiguedad, por fecha de ult modif.
+    foreach my $entry (@lisdir) {
+        next if (!-f "$dir/$entry");
+        my @stats = stat "$dir/$entry";
+        # print $stats[10] . ' ';
+        if ($stats[9] < (time - $segs_antig)) {
+            unlink "$dir/$entry";
+        }
+    }
+};
--- wizard_prontus/prontus_dir/cpan/core/editor_imag/js-local/EditorImag.class.js
+++ wizard_prontus/prontus_dir/cpan/core/editor_imag/js-local/EditorImag.class.js
@@ -1,9 +1,102 @@
 (function (window) {
     var self = null;
     var EditorImag = {
-        currentZoom: 0,
-        init: function (path_foto, w, h) {
-            $("#image").on('load', function () {
+        foto: null,
+        path_conf: null,
+        zoomRatio: 1,
+        imgElementId: "#image",
+        init: function (path_foto, w, h, path_conf) {
+            self = this;
+            self.foto = path_foto;
+            self.path_conf = path_conf;
+
+            self.methods.initCropper();
+            self.methods.initActions();
+            self.methods.initFotosFijas();
+        },
+        actions: {
+            apply: function (e) {
+                e.preventDefault();
+                var cropData = $(self.imgElementId).cropper('getData', true);
+                var imageData = $(self.imgElementId).cropper('getImageData');
+                var data = {
+                    srcX: cropData.x,
+                    srcY: cropData.y,
+                    width: cropData.width,
+                    height: cropData.height,
+                    aspectRatio:imageData.aspectRatio,
+                    rotate: cropData.rotate,
+                    foto: self.foto,
+                    zoomRatio: self.zoomRatio,
+                    _path_conf: self.path_conf
+                };
+
+                $.ajax({
+                    url: "/" + DIR_CGI_CPAN + "/prontus_editor_imag_guardar.cgi",
+                    type: "POST",
+                    dataType: "json",
+                    data: data,
+                    success: function(data) {
+                        if (data.status == '0') {
+                            alert(data.msg);
+                        } else {
+                            alert('ok.');
+
+                            parent.$('#_fotoeditada').val(data.msg);
+                            parent.Fid.submitir('Guardar', '_self');
+                        }
+                    },
+                    error: function() {
+                        alert('Ocurrió un error al procesar la imagen, inténtalo nuevamente.');
+                    }
+                });
+            },
+            move_down: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('move', 0, 10);
+            },
+            move_up: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('move', 0, -10);
+            },
+            move_right: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('move', 10, 0);
+            },
+            move_left: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('move', -10, 0);
+            },
+            reset: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('reset');
+            },
+            rotate_right: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('rotate', 90);
+            },
+            rotate_left: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('rotate', -90);
+            },
+            zoom_out: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('zoom', -0.1);
+            },
+            zoom_in: function (e) {
+                e.preventDefault();
+                $(self.imgElementId).cropper('zoom', 0.1);
+            },
+            cancel: function (e) {
+                e.preventDefault();
+                parent.$.colorbox.close();
+            },
+            saveZoomRatio: function (e) {
+                self.zoomRatio = e.ratio;
+            }
+        },
+        methods: {
+            initCropper: function () {
                 $('#image').cropper({
                     viewMode: 1,
                     dragMode: 'move',
@@ -14,93 +107,56 @@
                     cropBoxMovable: false,
                     cropBoxResizable: true,
                     minCropBoxWidth: 80,
-                    minCropBoxHeight: 80
+                    minCropBoxHeight: 80,
+                    zoom: self.actions.saveZoomRatio
                 });
-            });
+            },
+            initActions: function () {
+                $("#zoom_in").on("click", self.actions.zoom_in);
+                $("#zoom_out").on("click", self.actions.zoom_out);
+                $("#rotate_left").on("click", self.actions.rotate_left);
+                $("#rotate_right").on("click", self.actions.rotate_right);
+                $("#reset").on("click", self.actions.reset);
+                $("#move_left").on("click", self.actions.move_left);
+                $("#move_right").on("click", self.actions.move_right);
+                $("#move_up").on("click", self.actions.move_up);
+                $("#move_down").on("click", self.actions.move_down);
+                $("#apply").on("click", self.actions.apply);
+                $("#cancel").on("click", self.actions.cancel);
+            },
+            initFotosFijas: function () {
+                var fotoFijas = {};
 
-            $("#image").attr("src", path_foto);
-
-            $("#zoom_in").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('zoom', 0.1)
-            });
-
-            $("#zoom_out").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('zoom', -0.1)
-            });
-
-            $("#rotate_left").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('rotate', -90)
-            });
-
-            $("#rotate_right").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('rotate', 90);
-            });
-
-            $("#reset").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('reset');
-            });
-
-            $("#move_left").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('move', -10, 0);
-            });
-
-            $("#move_right").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('move', 10, 0);
-            });
-
-            $("#move_up").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('move', 0, -10);
-            });
-
-            $("#move_down").on("click", function (e) {
-                e.preventDefault();
-                $("#image").cropper('move', 0, 10);
-            });
-
-
-            var fotoFijas = {};
-
-            if (typeof parent.FotoFija.foto !== 'undefined') {
-                fotoFijas = parent.FotoFija.foto.instances;
-            }
-
-            console.log(fotoFijas);
-
-            $.each(fotoFijas, function (i, v) {
-                if (v.id != "temporal1" && v.id != "temporal2") {
-                    var aspectRatio = parseFloat(v.maxW / v.maxH).toFixed(2);
-                    var w = 50;
-                    var h = 50;
-
-                    if (aspectRatio) {
-                        if (h * aspectRatio > w) {
-                            h = Math.round(w / aspectRatio);
-                        } else {
-                            w = Math.round(h * aspectRatio);
-                        }
-                    }
-
-                    $(".fotos-fijas").append('<div class="box" id="'+ v.id + '" data-aspectratio="' + aspectRatio + '"><div class="size">' + v.maxW + 'x' + v.maxH +
-                                             '</div><div class="modelo" style="width:' + w + 'px;height:' + h + 'px;"></div></div>');
-
-                    console.log(w, h);
-
+                if (typeof parent.FotoFija.foto !== 'undefined') {
+                    fotoFijas = parent.FotoFija.foto.instances;
                 }
-            });
 
-            $(".fotos-fijas .box").on('click', function (e) {
-                var aspectRatio = $(this).data("aspectratio");
-                $("#image").cropper("setAspectRatio", aspectRatio);
-            });
+                $.each(fotoFijas, function (i, v) {
+                    if (v.id != "temporal1" && v.id != "temporal2") { // Se usan en la galeria. Se ignoran.
+                        var aspectRatio = parseFloat(v.maxW / v.maxH).toFixed(2);
+                        var w = 50;
+                        var h = 50;
 
+                        if (aspectRatio) {
+                            if (h * aspectRatio > w) {
+                                h = Math.round(w / aspectRatio);
+                            } else {
+                                w = Math.round(h * aspectRatio);
+                            }
+                        }
+
+                        $(".fotos-fijas").append('<div class="box" id="'+ v.id + '" data-aspectratio="' + aspectRatio + '"><div class="modelo" style="width:' + w + 'px;height:' + h + 'px;"></div></div>');
+                    }
+                });
+
+                $(".fotos-fijas .box").on('click', function (e) {
+                    var aspectRatio = $(this).data("aspectratio");
+
+                    $(".fotos-fijas .box").removeClass('active');
+                    $(self.imgElementId).cropper("setAspectRatio", aspectRatio);
+                    $(this).addClass('active');
+                });
+            }
         }
     };
     window.EditorImag = EditorImag;
