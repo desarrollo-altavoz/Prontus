55a93f88b0288b0f42784d02764e9a01a45a8f1f
Eduardo Alarcón <ealarcon@altavoz.net>
2018-01-02 21:08:27 -0300

    Mueve logica de validacion de contraseña a prontus auth

--- cgi-cpn/prontus_auth.pm
+++ cgi-cpn/prontus_auth.pm
@@ -10,8 +10,20 @@
 # ---------------------------------------------------------------
 
 package prontus_auth;
+use strict;
 use Digest::MD5 qw(md5_hex);
 
+our $USERS_USR;
+our $USERS_PSW;
+our $USERS_NOM;
+our $USERS_PERFIL;
+our $USERS_EMAIL;
+our $USERS_EXP_DAYS;
+our $USERS_FEC_EXP;
+
+our %FORM;
+
+# ---------------------------------------------------------------
 sub check_password {
     my $input_password = $_[0];
     my $stored_password = $_[1];
@@ -34,7 +46,7 @@ sub check_password {
 
     return 0;
 };
-
+# ---------------------------------------------------------------
 sub if_passwd_expired {
     my $fec_exp = $_[0];
 
@@ -44,13 +56,13 @@ sub if_passwd_expired {
         return 0;
     }
 };
-
+# ---------------------------------------------------------------
 sub check_if_new_hash {
     my $username = $_[0];
     my $hash = $_[1];
 
-    foreach $key (keys %prontus_varglb::USERS) {
-        $value = $prontus_varglb::USERS{$key};
+    foreach my $key (keys %prontus_varglb::USERS) {
+        my $value = $prontus_varglb::USERS{$key};
         my ($users_nom, $users_usr, $users_psw, $users_perfil) = split /\|/, $value;
 
         if (($users_usr eq $username)) {
@@ -64,7 +76,7 @@ sub check_if_new_hash {
 
     return $hash;
 };
-
+# ---------------------------------------------------------------
 sub encrypt_password_bcrypt {
     my $password = shift;
 
@@ -77,7 +89,7 @@ sub encrypt_password_bcrypt {
         return md5_hex($password);
     }
 };
-
+# ---------------------------------------------------------------
 sub generate_salt {
     my $itoa64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     my $salt = '';
@@ -85,7 +97,7 @@ sub generate_salt {
 
     return en_base64($salt);
 };
-
+# ---------------------------------------------------------------
 # Para el nuevo tipo de contraseña se usa solo el hash, no todo el string.
 sub get_hash_for_cookie {
     my $hash = $_[0];
@@ -98,7 +110,7 @@ sub get_hash_for_cookie {
         return $hash;
     }
 };
-
+# ---------------------------------------------------------------
 # Para retrocompatibilidad.
 sub load_module {
     return 1 if ($Crypt::Eksblowfish::Bcrypt::VERSION); # ya estaba cargando en la misma ejecucion.
@@ -112,7 +124,7 @@ sub load_module {
         return 1;
     }
 };
-
+# ---------------------------------------------------------------
 sub generate_password {
     my $length = $_[0];
     my $chars1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; # al menos 1 en mayuscula.
@@ -136,5 +148,84 @@ sub generate_password {
 
     return $str;
 }
+# ---------------------------------------------------------------
+sub user_valido {
+# retorna:
+# 0: user no valido normal
+# 1: user valido normal
+# -1 : user no valido por existir prontus_flag_sysadmin.txt
+# 2: user valido luego de comprobar clave contra prontus_flag_sysadmin.txt
+
+    my ($usr, $psw) = @_;
+
+    # Si esta este archivo, solo deja pasar con user admin y la pass contenida en el.
+    # Los demas usuarios son bloqueados.
+    my $flag_sysadmin = "$prontus_varglb::DIR_SERVER$prontus_varglb::DIR_DBM/users/prontus_flag_sysadmin.txt";
+    if (-f $flag_sysadmin) {
+        my $pass_sysadmin = &glib_fildir_02::read_file($flag_sysadmin);
+        if (($psw eq $pass_sysadmin) && ($usr eq 'admin')) {
+            $USERS_USR = $usr;
+            $USERS_PSW = &pronntus_auth::encrypt_password_bcrypt($pass_sysadmin);
+            return 2;
+        } else {
+            return -1;
+        }
+    }
+
+    foreach my $key (keys %prontus_varglb::USERS) {
+        my $val = $prontus_varglb::USERS{$key};
+        ($USERS_NOM, $USERS_USR, $USERS_PSW, $USERS_PERFIL, $USERS_EMAIL, $USERS_EXP_DAYS, $USERS_FEC_EXP) = split /\|/, $val;
+        # print STDERR "\n\nTIPEADA[$psw] USERS_PSW[$USERS_PSW] CRYPT[" . crypt($psw, $USERS_PSW) . "]\n" if ($USERS_USR eq $usr);
+        my $crypted_pass = '';
+
+        if (length $USERS_PSW == 60) {
+            if (($USERS_USR eq $usr) && &check_password($psw, $USERS_PSW)) {
+              return 3 if (&if_passwd_expired($USERS_FEC_EXP) && $USERS_FEC_EXP ne '' && $USERS_EXP_DAYS > 0 && $key != 1); # se excluye al admin.
+              return 1;
+            }
+        }
+
+        if (length($USERS_PSW) == 32) {
+            $crypted_pass = md5_hex($psw);
+        } else {
+            $crypted_pass = crypt($psw, $USERS_PSW);
+        }
+        if (($USERS_USR eq $usr) && ($crypted_pass eq $USERS_PSW)) {
+            return 3 if (&if_passwd_expired($USERS_FEC_EXP) && $USERS_FEC_EXP ne '' && $USERS_EXP_DAYS > 0 && $key != 1);
+            return 1;
+        }
+    }
+
+    # si esta presente este archivo, permite entrar con admin/prontus y luego obliga a cambiar la pass
+    my ($flag_file) = "$prontus_varglb::DIR_SERVER$prontus_varglb::DIR_DBM/users/prontus_flag_admin.txt";
+    if ( (!(-f $flag_file)) && ($usr eq 'admin') && ($psw eq 'prontus') ) {
+        # Resetear clave admin
+        ($USERS_NOM, $USERS_USR, $USERS_PSW, $USERS_PERFIL, $USERS_EMAIL) = split /\|/, $prontus_varglb::USERS{'1'}; # para rescatar email
+
+        $prontus_varglb::USERS{'1'} = 'Administrador|admin|' . &encrypt_password_bcrypt('prontus') . '|A|' . $USERS_EMAIL;
+        ($USERS_NOM, $USERS_USR, $USERS_PSW, $USERS_PERFIL, $USERS_EMAIL) = split /\|/, $prontus_varglb::USERS{'1'};
+
+        # USERS
+        my $linea = '';
+        my $buffer = '';
+        my $k;
+        foreach $k (keys %prontus_varglb::USERS) {
+            my $users_id = $k;
+            my ($users_nom, $users_usr, $users_psw, $users_perfil, $users_email) = split /\|/, $prontus_varglb::USERS{$k};
+            $linea = $users_id . '|' . $users_nom . '|' . $users_usr . '|' . $users_psw . '|' . $users_perfil . '|' . $users_email . "\n";
+            $buffer .= $linea;
+        }
+        if ($buffer) {
+            open (ARCHIVO,">$prontus_varglb::DIR_SERVER$prontus_varglb::DIR_DBM/users/users.txt") || return 0;
+            print ARCHIVO $buffer;
+            close ARCHIVO;
+        }
+
+        &glib_fildir_02::write_file("$prontus_varglb::DIR_SERVER$prontus_varglb::DIR_DBM/users/prontus_flag_admin.txt", 'No server domain, please try later.');
+        return 1;
+    }
+    return 0;
+};
+
 
 return 1;
