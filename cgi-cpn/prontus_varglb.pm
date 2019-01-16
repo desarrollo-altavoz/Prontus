6c476d6db89cc5298ef3463ec6a085f0863e9f25
Eduardo Alarcón <ealarcon@altavoz.net>
2019-01-16 18:56:00 -0300

    Corrige fecha de release

--- cgi-cpn/prontus_varglb.pm
+++ cgi-cpn/prontus_varglb.pm
@@ -46,7 +46,7 @@ sub init { # Prontus 6.0
   # $DIR_CGI_PUBLIC = 'cgi-bin'; # 1.13
   do 'dir_cgi.pm';
 
-  $VERSION_PRONTUS = '11.2.103 - 18/01/2019';
+  $VERSION_PRONTUS = '11.2.103 - 16/01/2019';
   $RAMA_INSTALADA = '';
   $NRO_REVISION_INSTALADA = '';
   $BETA_REVISION_INSTALADA = '';
